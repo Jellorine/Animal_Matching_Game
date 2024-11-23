296020dc97eabf59fdb689d3fea52b9fed6b1505
Jellorine <110653373+Jellorine@users.noreply.github.com>
2024-11-23 20:54:28 +0000

    Animal.js structure created

---
+++ animal_matching_game/src/components/Animal.js
@@ -0,0 +1,25 @@
+export default function Animal(props) {
+  
+  return (
+    <>
+      <div className="left-div">
+        <h3>Result</h3>
+        
+      </div>
+      <div className="middle">
+        <h3>Animal Name</h3>
+        
+      </div>
+      <div className="right-div">
+        <div className="selectAni">
+          <h3>Select an Animal</h3>
+          
+        </div>
+
+        <div className="animal-grid">
+          
+        </div>
+      </div>
+    </>
+  );
+}
