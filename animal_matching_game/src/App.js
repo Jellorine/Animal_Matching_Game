1f705d36f7cef4cdd5c692cc8bb416c4a55bc930
Jellorine <110653373+Jellorine@users.noreply.github.com>
2024-11-23 20:57:59 +0000

    structuring App.js

--- animal_matching_game/src/App.js
+++ animal_matching_game/src/App.js
@@ -6,6 +6,9 @@ import "./assets/css/style.css";
 function App() {
   return (
     <div>
+      <div className="header">
+        <h1>Animal Matching Game</h1>
+      </div>
       <div className="container">
         <Animal animal={animals}></Animal>
       </div>
